57208f4adaa7cf53192221bd15e2efd762b8da2f
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-07-23 01:09:53 +0900

    15.0.py
        #this part is main point
        email = pieces[1]               #extract all email address(ex. a@a.com)
        org = email.split('@')[1]      #split and get domain (since the question is to count domain)

---
+++ 15.0.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect('ex_15.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    
+    #this part is main point
+    email = pieces[1]               #extract all email address(ex. a@a.com)
+    org = email.split('@')[1]       #split and get domain (since the question is to count domain) 
+
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    conn.commit()
+
+# https://www.sqlite./lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
