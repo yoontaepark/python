d5763cb1be050b1a457674e5263ee5f19a2cff2e
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2020-07-22 23:10:18 +0900

    15.3.sql
    **To execute DB Browser for SQLite, click this below link and d/l
    https://sqlitebrowser.org/dl/

---
+++ 15.3.sql
@@ -0,0 +1,8 @@
+#SQL Summary
+
+1) INSERT INTO Users(name, email) VALUES('Kristin', 'kf@umich.edu')
+2) DELETE FROM Users WHERE email='fred@umich.edu'
+3) UPDATE Users SET name='Charles' WHERE email='csev@umich.edu'    #either "" or '' works well
+4) SELECT * FROM Users                                             #select all data in Users
+5) SELECT * FROM Users WHERE email='csev@umich.edu'
+6) SELECT * FROM Users ORDER BY email
